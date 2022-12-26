147399514ada25ccc91cbe271571b9b095557c4c
Aaron Y <101907461+chunloy@users.noreply.github.com>
2022-12-25 22:29:52 -0800

    implement blinking LED logic

--- src/main.cpp
+++ src/main.cpp
@@ -1,9 +1,17 @@
 #include <Arduino.h>
 
 void setup() {
-  // put your setup code here, to run once:
+  // set baud rate
+  Serial.begin(115200);
+
+  // set builtin LED as output
+  pinMode(LED_BUILTIN, OUTPUT);
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
+  // turn LED on and off every 1s
+  delay(1000);
+  digitalWrite(LED_BUILTIN, HIGH);
+  delay(1000);
+  digitalWrite(LED_BUILTIN, LOW);
 }
